e64d255016b47e5ca68410af14dd20c19e097231
Majrul Ansari <majrul.ansari@lntinfotech.com>
2020-12-18 12:06:58 +0530

    i also changed the greeting..

--- MyProj/src/HelloWorld.java
+++ MyProj/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Namaste");
 		System.out.println("Hello Again");
 		System.out.println("Hi");
 	}
