ad777aa524a91613cc79d8724c62d92292c5c4cd
Majrul Ansari <majrul.ansari@lntinfotech.com>
2020-12-18 12:06:28 +0530

    changed the greeting..

--- MyProj/src/HelloWorld.java
+++ MyProj/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Bonjour");
 		System.out.println("Hello Again");
 		System.out.println("Hi");
 	}
